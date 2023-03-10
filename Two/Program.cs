6bf953d3e34cde4112c84f74bc637de8c39c47e1
Alexander <fiireex@yandex.ru>
2023-03-10 20:03:32 +0300

    Done Two

---
+++ Two/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("ТАДАМ! А СЕЙЧАС Я ПОКАЖУ МАГИЮ! ЕСЛИ ЧИСЛО БУДЕТ НЕ МЕНЬШЕ ЧЕМ ТРЁХЗНАЧНОЕ, ТО Я ПОКАЖУ ТЕБЕ ТРЕТЬЮ ЦИФРУ!");
+Console.WriteLine("Вжууух!");
+int a = new Random().Next(10,10000000);
+Console.WriteLine($"Твоё число = {a}");
+while(a > 1000)
+    a = a/10;
+Console.WriteLine(a%10);
